c5d568e7643dc8cb2779df5de64cede6c0387475
William Pickering <willpicky@gmail.com>
2017-04-07 13:14:45 +0100

    Config file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DB: {
+    test: 'mongodb://localhost/northcoders-news-api-test',
+    dev: 'mongodb://will:admin@ds155150.mlab.com:55150/northcoders-news-api'
+  },
+  PORT: {
+    test: 3090,
+    dev: 3000
+  }
+};
